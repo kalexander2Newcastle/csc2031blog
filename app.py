64338f65681dc63cae7a3fac138bb97d8aa0d7d3
Kevin <k.alexander2@newcastle.ac.uk>
2022-10-11 15:44:17 +0100

    completed task 2

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import secrets
+
 from flask import Flask
 from blog.views import blog_blueprint
 from main.views import main_blueprint
 from users.views import users_blueprint
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 app.register_blueprint(blog_blueprint)
 app.register_blueprint(main_blueprint)
--- users/views.py
+++ users/views.py
@@ -1,11 +1,26 @@
 from flask import Blueprint, render_template
+from users.forms import RegisterForm
+from flask import request, redirect, url_for
+from wtforms.validators import DataRequired
+from wtforms.validators import Email
+from wtforms.validators import Length
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
 
-@users_blueprint.route('/register')
+@users_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('users/register.html')
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        print(request.form.get('username'))
+        # username = username(validators=[DataRequired()])
+        print(request.form.get('password'))
+        # password = PasswordField(validators=[Length(min=8, max=15)])
+        return redirect(url_for('users.login'))
+
+    return render_template('users/register.html', form=form)
+
 
 
 @users_blueprint.route('/login')
